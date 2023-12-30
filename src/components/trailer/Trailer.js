06af477b04aa2fbc72e626607ae27820c5a3f3f7
Duy <diidduong@gmail.com>
2023-12-30 14:21:40 -0700

    fix code indents

--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -4,13 +4,13 @@ import { useParams } from 'react-router-dom';
 import './Trailer.css';
 
 const Trailer = () => {
-    let params = useParams()
-    let youtubeID = params.youtubeID;
+  let params = useParams()
+  let youtubeID = params.youtubeID;
 
   return (
-    <div className='react-player-container'>
-        {youtubeID!=null?<ReactPlayer url={`https://www.youtube.com/watch?v=${youtubeID}`} playing={true} width='100%' height='100%'/>:null}
-    </div>
+  <div className='react-player-container'>
+    {youtubeID!=null?<ReactPlayer url={`https://www.youtube.com/watch?v=${youtubeID}`} playing={true} width='100%' height='100%'/>:null}
+  </div>
   )
 }
 
